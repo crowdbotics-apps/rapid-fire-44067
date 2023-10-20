2f6fff97c0d8706469beeea9e6af3bd99a658ae8
Crowdbotics <team@crowdbotics.com>
2023-10-20 21:22:26 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "rapid-fire-44067",
+  url: "https://rapid-fire-44067.botics.co",
+  api: "https://rapid-fire-44067.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
